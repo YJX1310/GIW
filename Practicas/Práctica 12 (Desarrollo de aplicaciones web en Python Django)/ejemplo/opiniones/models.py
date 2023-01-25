a6f3ceeaf4928390deea3e21b8d7794a402e288e
YuejieXuUCM <xuyuejie45@gmail.com>
2023-01-25 18:48:17 +0100

    Practica 12

---
+++ Practicas/Práctica 12 (Desarrollo de aplicaciones web en Python Django)/ejemplo/opiniones/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.html import escape
+
+
+class Opinion(models.Model):
+    """Modelo para almacenar la opinion de un usuario"""
+    # Identificador único autogenerado
+    id = models.BigAutoField(primary_key=True)
+    
+    # Puntuación entre 0 y 10
+    puntuacion = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+
+    # Texto de máximo 200 caracteres
+    texto = models.CharField(max_length=200)
+
+    # Al crear una opinion se pone la fecha actual
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    # Referencia a un usuario del sistema. Si se elimina el usuario, este campo se pone a NULL
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+    def clean(self):
+        """Escapa el texto por si contiene caracteres HTML"""
+        self.texto = escape(self.texto)
+
+    def __str__(self):
+        """Para mostrar detalles en la interfaz admin"""
+        return f"Opinión({self.pk}, {self.texto}, {self.autor})"
